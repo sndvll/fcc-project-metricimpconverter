1c5ae31a0a2ef9102894403f986a375e3a044a69
Glitch (sndvll-fcc-project-metricimpconverter) <none>
2019-07-31 17:48:40 +0000

    🛎🎄 Checkpoint ./server.js:5687688/189 ./tests/1_unit-tests.js:5687688/236

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 'use strict';
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const expect      = require('chai').expect;
+const cors        = require('cors');
+const helmet = require('helmet');
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
 
-var app = express();
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
+
+const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -17,6 +19,11 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet({
+  hidePoweredBy: true,
+  noSniff: true,
+  xssFilter: true
+}));
 
 //Index page (static HTML)
 app.route('/')
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,30 +6,32 @@
 *       (if additional are added, keep them at the very end!)
 */
 
-var chai = require('chai');
-var assert = chai.assert;
-var ConvertHandler = require('../controllers/convertHandler.js');
+const chai = require('chai');
+const assert = chai.assert;
+const ConvertHandler = require('../controllers/convertHandler.js');
 
-var convertHandler = new ConvertHandler();
+const convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
   
   suite('Function convertHandler.getNum(input)', function() {
     
     test('Whole number input', function(done) {
-      var input = '32L';
+      const input = '32L';
       assert.equal(convertHandler.getNum(input),32);
       done();
     });
     
     test('Decimal Input', function(done) {
-      
-      //done();
+      const input = '5.5L';
+      assert.equal(convertHandler.getNum(input), 5.5);
+      done();
     });
     
     test('Fractional Input', function(done) {
-      
-      //done();
+      const input = '54/2L';
+      assert.equal(convertHandler.getNum(input), '54/2');
+      done();
     });
     
     test('Fractional Input w/ Decimal', function(done) {
